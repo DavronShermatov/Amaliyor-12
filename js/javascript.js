aa15050a5461001561549e34344745b0903599d2
Shermatov Davron Devloper full stack <93197110+DavronShermatov@users.noreply.github.com>
2022-06-24 18:42:30 +0300

    javascript code

--- js/javascript.js
+++ js/javascript.js
@@ -1,3 +1,19 @@
-// 1 masala
-numberOfSerius = prompt("nechta serial ko'rdingiz", "15");
+//  1 masala
+const numberOfSerius = prompt("nechta serial ko'rdingiz", "15");
 console.log(numberOfSerius);
+//  2 masala
+const seriesDB = {
+    count:numberOfSerius,
+    series:{},
+    actors:{},
+    genres:[],
+    privat:false
+};
+// 3 masala
+const a = prompt("oxirgi ko'rgan serialingiz", "Umar Ibn Hattob"),
+b = +prompt("Nechta serial ko'rgansiz" ,"10"),
+d = prompt("oxirgi ko'rgan serialingiz", "Ahmad Ibn Hanbai"),
+c = +prompt("Nechta serial ko'rgansiz","10");
+ seriesDB.series[a]=b;
+ seriesDB.series[d]=c;
+ console.log(seriesDB);
